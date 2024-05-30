bbec9843821792abb402969405acc2a1cea8ffd3
Mayane Lopes <mayaneclopes@gmail.com>
2024-05-30 15:54:22 -0300

    início do projeto React

---
+++ aula-tads/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="title">Primeiro projeto em ReactJs</h1>
+      <img src="https://w7.pngwing.com/pngs/1010/842/png-transparent-corinthians-hd-logo.png"
+        alt="logo do corinthians"
+        width={300}
+      />
+    </div>
+  );
+}
+
+export default App;
